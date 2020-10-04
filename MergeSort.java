f65290a9d2c76c661d4ebbdbc67025c1fe4c11ec
rheeshabh kumar singh <rishabhs861@gmail.com>
2020-10-04 18:11:43 +0530

    merge sort implementation completed

--- MergeSort.java
+++ MergeSort.java
@@ -12,14 +12,18 @@ public class MergeSort {
 			mergeSort(A,q+1,r);
 			merge(A,p,q,r);
 		}
+		for(int i:A)
+		{
+			System.out.print(A[i]);
+		}
 	}
 	
 	static void merge(int[] A,int p, int q, int r)
 	{
 		int n1= q-p+1;
 		int n2= r-q;
-		int[] L= new int[n1];
-		int[] R= new int[n2];
+		int[] L= new int[n1+1];
+		int[] R= new int[n2+1];
 		
 		for(int i=0;i<n1; i++)
 		{
@@ -30,7 +34,26 @@ public class MergeSort {
 			R[j]=A[q+1+j];
 		}
 		
+		L[n1+1]=999999999;
+		R[n2+1]=999999999;
 		
+		int i=0,j=0;
+		
+		for(int k=p;k<=r;k++)
+		{
+			if(L[i]<=R[j])
+			{
+				A[k]=L[i];
+				i++;
+			}
+			else
+			{
+				A[k]=R[j];
+				j++;
+			}
+		}
 	}
+	
+	
 
 }
