7482f609918d94dc3e91486bf04b352888bfce16
rheeshabh kumar singh <rishabhs861@gmail.com>
2020-10-07 20:37:30 +0530

    added quick sort reference

--- DivideAndConquerStrategy.java
+++ DivideAndConquerStrategy.java
@@ -6,6 +6,13 @@ public class DivideAndConquerStrategy {
 		int[] arr= new int[] {2,4,-1,6,-9,-65,34,99,23,100};
 		FindMinMax fmm= new FindMinMax();
 		fmm.minMax(arr);
+		
+		MergeSort ms= new MergeSort();
+		ms.mergeSort(arr, 0, 9);
+		
+		
+		QuickSort quicksort=new QuickSort();
+		quicksort.quickSort(arr, 0, arr.length-1);
 	}
 
 }
